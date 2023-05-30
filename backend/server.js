6adf21123e0da6c206f2dec67d3e3461959e0ecc
ArpitGorasiya <126314689+ArpitGorasiya@users.noreply.github.com>
2023-05-30 11:37:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,7 @@
 const app = require("./app");
 const connectDatabase = require("./db/Database");
 
-// Handling uncaught Exception
-process.on("uncaughtException", (err) => {
-  console.log(`Error: ${err.message}`);
-  console.log(`shutting down the server for handling uncaught exception`);
-});
-
-// config
-if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({
-    path: "config/.env",
-  });
-}
+  require("dotenv").config()
 
 // connect db
 connectDatabase();
@@ -24,12 +13,3 @@ const server = app.listen(process.env.PORT, () => {
   );
 });
 
-// unhandled promise rejection
-process.on("unhandledRejection", (err) => {
-  console.log(`Shutting down the server for ${err.message}`);
-  console.log(`shutting down the server for unhandle promise rejection`);
-
-  server.close(() => {
-    process.exit(1);
-  });
-});
